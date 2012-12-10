4978adb631ca0431dca54680a1f4a52e6b1c5ea9
sonnym <michaud.sonny@gmail.com>
2012-12-09 22:31:23 -0500

    fix issue with vocabulary factory machine name

--- tests/factories/taxonomy.php
+++ tests/factories/taxonomy.php
@@ -1,6 +1,6 @@
 <?php
 Factory::define('vocabulary', 'vocab', array(
-  'name' => Factory::sequence(function ($n) { return "taxonomy_name{$n}"; }),
-  'machine_name' => Factory::sequence(function ($n) { "taxonomy_machine_name{$n}"; }),
+  'name' => Factory::sequence(function ($n) { return "vocabulary_name{$n}"; }),
+  'machine_name' => Factory::sequence(function ($n) { return "vocabulary_machine_name{$n}"; }),
   'heirarchy' => 0,
 ));
